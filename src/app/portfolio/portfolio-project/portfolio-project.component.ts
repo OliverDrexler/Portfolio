5fd8de67a062f8884a1f3a2e89058cf7792058bc
Oliver Drexler <oliverdrexler@web.de>
2024-06-30 13:31:17 +0200

    added logic for images

--- src/app/portfolio/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio/portfolio-project/portfolio-project.component.ts
@@ -10,4 +10,17 @@ import { Component, Input } from '@angular/core';
 })
 export class PortfolioProjectComponent {
   @Input() isReverse: boolean = false;
+
+  imgSrcNormal = '../../../assets/img/04_projects/join.png';
+  imgAltNormal = 'Join';
+  imgSrcReverse = '../../../assets/img/04_projects/pl.png';
+  imgAltReverse = 'El Pollo Loco';
+
+  getImgSrc() {
+    return this.isReverse ? this.imgSrcReverse : this.imgSrcNormal;
+  }
+
+  getImgAlt() {
+    return this.isReverse ? this.imgAltReverse : this.imgAltNormal;
+  }
 }
