2b222453437d9951db02aa3c1ae4ddf9f9b91f9a
Oliver Drexler <oliverdrexler@web.de>
2024-06-30 14:27:02 +0200

    added number count to projects

--- src/app/portfolio/portfolio-project/portfolio-project.component.ts
+++ src/app/portfolio/portfolio-project/portfolio-project.component.ts
@@ -6,21 +6,41 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './portfolio-project.component.html',
-  styleUrl: './portfolio-project.component.scss'
+  styleUrl: './portfolio-project.component.scss',
 })
 export class PortfolioProjectComponent {
   @Input() isReverse: boolean = false;
+  @Input() currentProject: number = 1;
+  @Input() totalProjects: number = 1;
 
   imgSrcNormal = '../../../assets/img/04_projects/join.png';
   imgAltNormal = 'Join';
   imgSrcReverse = '../../../assets/img/04_projects/pl.png';
   imgAltReverse = 'El Pollo Loco';
 
+  /**
+   * Returns the appropriate image source based on the isReverse flag.
+   * @returns {string} The image source URL.
+   */
   getImgSrc() {
     return this.isReverse ? this.imgSrcReverse : this.imgSrcNormal;
   }
 
+  /**
+   * Returns the appropriate alt text based on the isReverse flag.
+   * @returns {string} The alt text for the image.
+   */
   getImgAlt() {
     return this.isReverse ? this.imgAltReverse : this.imgAltNormal;
+  }
+
+  /**
+   * Formats a number to ensure it is at least two digits.
+   * If the number is less than 10, it prefixes the number with a zero.
+   * @param {number} num - The number to format.
+   * @returns {string} The formatted number as a string.
+   */
+  formatNumber(num: number): string {
+    return num < 10 ? `0${num}` : `${num}`;
   }
 }
