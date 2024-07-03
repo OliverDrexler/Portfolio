f44185214dcebc68baef79cdb01341acd55559a8
Oliver Drexler <oliverdrexler@web.de>
2024-07-03 07:56:19 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [
-  { path: '', component: LandingPageComponent },
-  { path: 'about', component: AboutMeComponent },
-  { path: 'skills', component: SkillsComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'contact', component: ContactComponent },
-];
+export const routes: Routes = [];
