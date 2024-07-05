264c8ae0fff7d209abcdba029824cfbcf3ad2cce
Oliver Drexler <oliverdrexler@web.de>
2024-07-05 16:56:36 +0200

    Update sendMail.php

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'oliverdrexler@web.de';  
+            $recipient = 'olidrexler@gmail.com';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
