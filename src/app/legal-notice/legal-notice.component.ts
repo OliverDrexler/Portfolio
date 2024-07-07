dbbb7945300dcf8426002ae45d2ecb4b2bc63427
Oliver Drexler <oliverdrexler@web.de>
2024-07-07 17:20:19 +0200

    finalized legal notice

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  constructor(
+    private translate: TranslateService,
+    private viewportScroller: ViewportScroller
+  ) {}
 
+  ngOnInit(): void {
+    this.scrollToTop();
+  }
+
+  scrollToTop(): void {
+    this.viewportScroller.scrollToPosition([0, 0]);
+  }
 }
